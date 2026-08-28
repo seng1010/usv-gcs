e16c95719b4c354b8500dbc77b483f8e77b41b40
parkseeun <parkse1010@gmail.com>
2026-08-28 11:28:44 +0900

    Sync GUI/dashboard from team scaffold, confirm joystick axis mapping
    - gui_main_node, dashboard_html, gcs.launch.py 팀 저장소(6can_usv_ws)에서 가져옴
    - joy_to_cmd_node는 기존 구현(deadzone, heartbeat 옵션) 유지
    - Xbox 360 컨트롤러 실측으로 조이스틱 축 번호/부호 확정 (linear_axis=1, angular_axis=0)

--- src/usv_gcs/setup.py
+++ src/usv_gcs/setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +24,7 @@ setup(
     entry_points={
         'console_scripts': [
             'joy_to_cmd_node = usv_gcs.joy_to_cmd_node:main',
+            'gui_main_node = usv_gcs.gui_main_node:main',
         ],
     },
 )
